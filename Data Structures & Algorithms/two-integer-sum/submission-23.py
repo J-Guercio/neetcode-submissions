149e80291ac96ddb01e84ae890f07607751c76fc
J-Guercio <137239110+J-Guercio@users.noreply.github.com>
2026-07-13 11:31:38 -0500

    Add: two-integer-sum - submission-23

---
+++ Data Structures & Algorithms/two-integer-sum/submission-23.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = dict()
+        for i,x in enumerate(nums):
+            complament = target - x
+            if complament in seen:
+                return list((seen[complament],i))
+            else:
+                seen[x] = i
